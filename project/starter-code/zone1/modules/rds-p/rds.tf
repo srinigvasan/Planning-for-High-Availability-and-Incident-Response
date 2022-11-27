3fc625cca1f1b77b3ef4f4772f6a18ccb80be795
Srini <srinigvasan@gmail.com>
2022-11-27 21:45:36 +0800

    add 3 ec2

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -55,7 +55,9 @@ output "db_cluster_arn" {
 }
 
 output "db_instance_arn" {
-  value = aws_rds_cluster_instance.udacity_instance[0].arn
+
+  value = aws_rds_cluster_instance.udacity_instance[1].writer ? aws_rds_cluster_instance.udacity_instance[1].arn : aws_rds_cluster_instance.udacity_instance[0].arn
+
 }
 
 resource "aws_rds_cluster_instance" "udacity_instance" {
