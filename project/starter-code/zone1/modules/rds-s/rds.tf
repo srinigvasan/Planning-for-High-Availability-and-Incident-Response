6e7364bf4b5c9f54adf716cc6b36a2af2ef847af
Srini <srinigvasan@gmail.com>
2022-12-01 08:07:30 +0800

    add rds

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -51,16 +51,16 @@ resource "aws_security_group" "db_sg_2" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 3306
-    protocol    = "TCP"
-    to_port     = 3306
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 3306
-    protocol    = "TCP"
-    to_port     = 3306
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
